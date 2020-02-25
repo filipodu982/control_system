4c51e98704b10ca002fa839b9a25c8119411cb33
filipodu982 <filipodu982@gmail.com>
2020-02-25 13:13:53 +0100

    First commit

---
+++ cmake-build-debug/CMakeFiles/control_system.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/control_system.dir/main.cpp.obj"
+  "control_system.exe"
+  "control_system.exe.manifest"
+  "control_system.pdb"
+  "libcontrol_system.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
