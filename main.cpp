b2a0da0c32df4d271a485328880b3a443b666afc
filipodu982 <filipodu982@gmail.com>
2020-02-25 13:18:31 +0100

    Added Eigen

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "Eigen/Dense"
+using namespace Eigen;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Vector4d x(3,4,5,6);
+    std::cout<<x<<std::endl;
+    DiagonalMatrix<double,4> m;
+    m.diagonal() = x;
+    std::cout << m.toDenseMatrix() << std::endl;
     return 0;
 }
